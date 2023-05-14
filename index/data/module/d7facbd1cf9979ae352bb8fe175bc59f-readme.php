b9a0474cadfade13a5f51368c5be4941766ebaaf
9117343 <133550237+9117343@users.noreply.github.com>
2023-05-14 20:57:00 +0800

    gugeplay

--- index/data/module/d7facbd1cf9979ae352bb8fe175bc59f-readme.php
+++
@@ -1,2 +0,0 @@
-<p style='line-height:150%'>织梦采集侠由dedeapps.com制作<br><br />
-织梦管理员之家http://www.dedeadmin.com赞助支持<br></p>
--- index/membe5555r/feedback.php
+++
@@ -1,4 +0,0 @@
-<?php
-$cfg_formmember = true;
-require_once(dirname(__FILE__).'/../plus/feedback.php');
-?>
